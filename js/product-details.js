844d6b9fb6ef462a9ec8d190c49fa60cd03e8fe7
= <=>
2019-10-13 14:25:53 +0700

    update contact.html, about.html, footer.html, product-details.html, product.html, footer.css, global.css, product-details.css, product.css, responsive.css, script.js, product.js, product-details.js, contact.js. Creat file cart.html, cart.css, cart.js

--- js/product-details.js
+++ js/product-details.js
@@ -5,7 +5,8 @@ jQuery(document).ready(function($) {
 
 	// Render
 	$.getJSON('products.json', function(data) {
-		let product = data[6].allProducts;
+		let product = data[6].allProducts;	
+
 		$(".content").html(`
 			<nav aria-label="breadcrumb">
 				<ol class="breadcrumb">
@@ -16,36 +17,52 @@ jQuery(document).ready(function($) {
 			</nav>
 
 			<div class="row product-details">
-				<div class="col-sm-5 col-12">
+				<div class="col-md-5 col-12">
 					<img src="${product[id-1].img2}" class="w-100" />
 				</div>
-				<div class="col-sm-4 col-12">
+				<div class="col-md-4 col-12">
 					<h4 class="product-details__name">${product[id-1].name}</h4>
-					<p>Tình trạng: <span class="text-success">còn hàng</span></p>
-					<p>Chất liệu: ${product[id-1].material}</p>
-					<p><span>Giá sản phẩm: </span><span class="product-details__price">${product[id-1].price} <sup>đ</sup></span></p>
+					<p class="product-details__text"><span class="product-details__desc">Tình trạng:</span> <span class="text-success">còn hàng</span></p>
+					<p class="product-details__text"><span class="product-details__desc">Chất liệu:</span> ${product[id-1].material}</p>
+					<p class="product-details__text"><span class="product-details__desc">Mô tả:</span> ${product[id-1].desc}</p>
+					<p class="product-details__text"><span class="product-details__desc">Giá sản phẩm: </span><span class="product-details__price">${product[id-1].price} <sup>đ</sup></span></p>
 					<form>
-					Số lượng: <input class="product-details__amount" name="amount" type="number" value="1"> <br>
+					<span class="product-details__desc">Số lượng: </span><input class="product-details__amount" name="amount" type="number" value="1"> <br>
 					<input class="product-details__submit" name="submit" type="submit" value="Mua ngay"> <br>
-					<input class="product-details__addToCart" name="add-to-cart" type="submit" value="Thêm vào giỏ hàng">
+					<input class="product-details__addToCart add-to-cart" name="add-to-cart" type="submit" value="Thêm vào giỏ hàng">
 					</form>
 					</div>
-				<div class="col-sm-3 col-12">
-					<div class="col-12 service">
-						<div class="text-center"><i class="fas fa-microphone-alt fa-2x"></i></div>
-						<p class="text-center">Tư vấn 24/7</p>
-					</div>
-					<div class="col-12 service">
-						<div class="text-center"><i class="fas fa-check-circle fa-2x"></i></div>
-						<p class="text-center">Đảm bảo chất lượng</p>
-					</div>
-					<div class="col-12 service">
-						<div class="text-center"><i class="fas fa-shipping-fast fa-2x"></i></div>
-						<p class="text-center">Giao hàng nhanh chóng</p>
+				<div class="col-md-3 col-12">
+					<div class="row mx-auto product-details__service">
+						<div class="col-md-12 col-sm-4 col-12 product-details__service1">
+							<div class="text-center"><i class="fas fa-microphone-alt fa-2x"></i></div>
+							<p class="text-center">Tư vấn 24/7</p>
+						</div>
+						<div class="col-md-12 col-sm-4 col-12 product-details__service2">
+							<div class="text-center"><i class="fas fa-check-circle fa-2x"></i></div>
+							<p class="text-center">Đảm bảo chất lượng</p>
+						</div>
+						<div class="col-md-12 col-sm-4 col-12 product-details__service3">
+							<div class="text-center"><i class="fas fa-shipping-fast fa-2x"></i></div>
+							<p class="text-center">Giao hàng nhanh chóng</p>
+						</div>
 					</div>
 				</div>
 			</div>
 		`)
 	});
+
+	// Back to top
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('.back-to-top').fadeIn(); 
+        } else { 
+            $('.back-to-top').fadeOut(); 
+        } 
+    }); 
+    $('.back-to-top').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
 
 });
--- js/script.js
+++ js/script.js
@@ -10,56 +10,57 @@ $(function() {
 		hotProducts.forEach( function(element, index) {
 			$("#hot-products").append(`
 				<div data-id="${element.id}" class="card col-12 col-md-4 card-${index}">
-					<div class="hot-products__img">
-						<a href="product-details.html#${element.id}" target="_blank" class="link-to-product"><img src="images/sp-noi-bat/${element.img}" class="card-img-top" alt="${element.name}" title="${element.name}"></a>
-					</div>
-					<div class="card-body text-center pt-2">
-						<div>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>					
-						</div>
-						<p class="card-title hot-products__title">${element.name}</p>
-						<hr>
-						<div>				       
-						<div class="card-text hot-products__price">${element.price} <sup>đ</sup></div>
-						<div class="hot-products__btn">
-							<a href="product-details.html#${element.id}" target="_blank" class="btn__buy-now link-to-product"">Mua ngay</a>
-							<a href="#" title="Thêm vào giỏ hàng"><i class="fas fa-cart-plus"></i></a>
-						</div>
-						</div>    
-					</div> 
+				<div class="hot-products__img">
+				<a href="product-details.html#${element.id}" target="_blank" class="link-to-product"><img src="images/sp-noi-bat/${element.img}" class="card-img-top" alt="${element.name}" title="${element.name}"></a>
 				</div>
-			`)
+				<div class="card-body text-center pt-2">
+				<div>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>					
+				</div>
+				<p class="card-title hot-products__title">${element.name}</p>
+				<hr>
+				<div>				       
+				<div class="card-text hot-products__price">${element.price} <sup>đ</sup></div>
+				<div class="hot-products__btn">
+				<a href="product-details.html#${element.id}" target="_blank" class="btn__buy-now link-to-product"">Mua ngay</a>
+				<a href="#" class="add-to-cart" title="Thêm vào giỏ hàng"><i class="fas fa-cart-plus"></i></a>
+				</div>
+				</div>    
+				</div> 
+				</div>
+				`)
 		});
 
-	// Đổ dữ liệu vào Sản phẩm khuyến mãi
+		// Đổ dữ liệu vào Sản phẩm khuyến mãi
 		let promotionalProducts = data[1].promotionalProducts;
 		promotionalProducts.forEach(function(element,index) {
 			$(".promotional-products__content").append(`
 				<div data-id="${element.id}" class="card mb-2 col-md-6 card-${index}">
-					<div class="row no-gutters">
-						<div class="col-md-6 promotional-products__img">
-							<a href="product-details.html#${element.id}" target="_blank" class="link-to-product"><img src="images/sp-khuyen-mai/${element.img}" class="card-img" alt="${element.name}" title="${element.name}"></a>
-						</div>
-						<div class="col-md-6">
-							<div class="card-body">
-								<h6 class="card-title promotional-products__title">${element.name}</h6>
-								<p class="card-text promotional-products__new-price">${element.newPrice} <sup>đ</sup></p>
-								<p class="card-text promotional-products__old-price">${element.oldPrice} <sup>đ</sup></p>
-								<div class="promotional-products__btn">
-									<a href="product-details.html#${element.id}" target="_blank" class="btn__buy-now link-to-product">Mua ngay</a>
-									<a href="#" title="Thêm vào giỏ hàng"><i class="fas fa-cart-plus"></i></a>
-								</div>
-							</div>
-						</div>
-					</div>
+				<div class="row no-gutters">
+				<div class="col-md-5 col-12 promotional-products__img">
+				<a href="product-details.html#${element.id}" target="_blank" class="link-to-product"><img src="images/sp-khuyen-mai/${element.img}" class="card-img" alt="${element.name}" title="${element.name}"></a>
 				</div>
-			`)
+				<div class="col-md-7 col-12">
+				<div class="card-body">
+				<h6 class="card-title promotional-products__title">${element.name}</h6>
+				<p class="card-text promotional-products__new-price">${element.newPrice} <sup>đ</sup></p>
+				<p class="card-text promotional-products__old-price">${element.oldPrice} <sup>đ</sup></p>
+				<div class="promotional-products__btn">
+				<a href="product-details.html#${element.id}" target="_blank" class="btn__buy-now link-to-product">Mua ngay</a>
+				<a href="#" class="add-to-cart" title="Thêm vào giỏ hàng"><i class="fas fa-cart-plus"></i></a>
+				</div>
+				</div>
+				</div>
+				</div>
+				</div>
+				`)
 		})
 	})
+	
 
 	// Slide Tin tức
 	$('.news__items').slick({
@@ -76,17 +77,17 @@ $(function() {
 
 
 	// Back to top
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('.back-to-top').fadeIn(); 
-        } else { 
-            $('.back-to-top').fadeOut(); 
-        } 
-    }); 
-    $('.back-to-top').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
+	$(window).scroll(function(){ 
+		if ($(this).scrollTop() > 100) { 
+			$('.back-to-top').fadeIn(); 
+		} else { 
+			$('.back-to-top').fadeOut(); 
+		} 
+	}); 
+	$('.back-to-top').click(function(){ 
+		$("html, body").animate({ scrollTop: 0 }, 600); 
+		return false; 
+	});
 
 
 	
